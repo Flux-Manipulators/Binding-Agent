1683324e532d97aa85bfa538509acf13022df192
Pickle String <picklestring@gmail.com>
2026-06-26 12:02:13 +1000

    fixes

--- common/src/main/java/net/picklestring/binding_agent/debug/BindingAgentDebug.java
+++ common/src/main/java/net/picklestring/binding_agent/debug/BindingAgentDebug.java
@@ -1,14 +1,10 @@
 package net.picklestring.binding_agent.debug;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.nbt.CompoundTag;
 import net.picklestring.binding_agent.Binding_agent;
 import net.picklestring.binding_agent.api.attachment.PlayerAttachmentRegistry;
-import net.picklestring.binding_agent.api.attachment.PlayerAttachmentSerializer;
 import net.picklestring.binding_agent.api.attachment.PlayerAttachmentType;
 import net.picklestring.binding_agent.command.BindingAgentTestCommand;
 import net.picklestring.binding_agent.inbuilt.IntAttachment;
-import net.picklestring.binding_agent.test.TestIntAttachment;
 
 public final class BindingAgentDebug {
     public static final PlayerAttachmentType<IntAttachment> MANA = PlayerAttachmentRegistry.register(
